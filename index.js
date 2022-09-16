294d75c9c69432b23a4376ebc7a025d04e427a70
krkmariia <krykunmariia@gmail.com>
2022-09-16 15:50:57 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+const dateOfBirth = 1984 ;
+let currentYear = 2022 ;
+let age = currentYear - dateOfBirth ;
+console.log('Введене число = ' + dateOfBirth);
+console.log('Наступне = ' + currentYear);
+console.log('Наступне = ' + age);
