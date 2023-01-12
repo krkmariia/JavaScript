b1cb8b27db5a8cf78644bca220d61d06edafd3df
krkmariia <61343459+krkmariia@users.noreply.github.com>
2023-01-12 20:04:11 +0100

    HW_14

--- index.js
+++ index.js
@@ -1,45 +1,76 @@
-const options = {
-    threshold: 0.5
+const btnForClock = document.querySelector("#clockCreateAndDelete");
+const clockHTML = document.querySelector("#clockHTML");
+const inputForTimer = document.querySelector("#inputForTimer");
+const timerHTML = document.querySelector("#timerHTML");
+
+let flag = 0;
+
+let youTime = {};
+
+let normalTime = (times)=>{
+    if(times.getHours() < 10){
+        youTime.hours = (`0${times.getHours()}`)
+    }
+    if(times.getMinutes() < 10){
+        youTime.minutes = (`0${times.getMinutes()}`)
+    }
+    if(times.getSeconds() < 10){
+        youTime.seconds = (`0${times.getSeconds()}`)
+    }
 };
 
-let arrNav = document.querySelectorAll('a');
+let clock = setInterval(()=>{
+    if(flag == 1){
+        var timeNow = new Date();
+        youTime.hours = timeNow.getHours();
+        youTime.minutes = timeNow.getMinutes();
+        youTime.seconds = timeNow.getSeconds();
+        normalTime(timeNow);
+        clockHTML.textContent = (`${youTime.hours} : ${youTime.minutes} : ${youTime.seconds}`)
+    }
+    else{
+        clearInterval(clock);
+        clockHTML.textContent = null;
+    }
+}, 1000);
 
-const observer = new IntersectionObserver((entries, observer) => {
-    entries.forEach(entry => {
-        if(entry.isIntersecting){
-            console.log(entry.target.id)
-            if (entry.target.id == 0){
-                arrNav.forEach(navItem => {
-                    if (navItem.id != 0){
-                        navItem.style.backgroundColor = 'grey'
-                    }
-                })
-                arrNav[0].style.backgroundColor = 'lightblue'
+btnForClock.addEventListener('click', ()=>{
+    if(flag == 0){
+        flag = 1;
+        clock = setInterval(()=>{
+            if(flag == 1){
+                var timeNow = new Date();
+                youTime.hours = timeNow.getHours();
+                youTime.minutes = timeNow.getMinutes();
+                youTime.seconds = timeNow.getSeconds();
+                normalTime(timeNow);
+                clockHTML.textContent = (`${youTime.hours} : ${youTime.minutes} : ${youTime.seconds}`)
             }
-            if (entry.target.id == 1){
-                arrNav.forEach(navItem => {
-                    if (navItem.id != 1){
-                        navItem.style.backgroundColor = 'grey'
-                    }
-                })
-                arrNav[1].style.backgroundColor = 'lightblue'
+            else{
+                clearInterval(clock);
+                clockHTML.textContent = null;
             }
-            if (entry.target.id == 2){
-                arrNav.forEach(navItem => {
-                    if (navItem.id != 2){
-                        navItem.style.backgroundColor = 'grey'
-                    }
-                })
-                arrNav[2].style.backgroundColor = 'lightblue'
-            }
-        }
-        else{
-
-        }
-    });
-}, options);
-
-let arrArcicle = document.querySelectorAll('article')
-arrArcicle.forEach(block => {
-    observer.observe(block);
+        }, 1000);
+        btnForClock.textContent = 'Delete';
+    }
+    else if(flag == 1){
+        flag = 0;
+        btnForClock.textContent = 'Create';
+    }
 });
+
+inputForTimer.addEventListener('change', ()=>{
+    if(inputForTimer.value > 0){
+        let timerValue = inputForTimer.value;
+        let intrval = setInterval(()=>{
+            console.log(timerValue);
+            timerHTML.textContent = timerValue;
+            timerValue -= 1;
+        },1000)
+        setTimeout(()=>{
+            alert('Время Вышло!');
+            clearInterval(intrval);
+            timerHTML.textContent = null;
+        },(inputForTimer.value*1000)+1000);
+    }
+})
